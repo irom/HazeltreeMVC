1207b1bccafde05997eee497a2136fe507536e34
Ivan <romanov75@gmail.com>
2015-10-29 13:09:46 -0400

    Hazeltree

---
+++ HazeltreeMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HazeltreeMVC.Models;
+
+namespace HazeltreeMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        Securities securities = new Securities();
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult SearchSecurities(string SecurityCode, string SearchText)
+        {
+            var result = securities.Search(SecurityCode, SearchText);
+            return PartialView("_SecurityLookup", result);
+        }
+    }
+}
---
+++ HazeltreeMVC/Models/Security.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HazeltreeMVC.Models
+{
+    [Serializable]
+    public class Security
+    {
+        public int SecurityId { get; set; }
+        public string SecurityCode { get; set; }
+        public string SecurityName { get; set; }
+        public string Sedol { get; set; }
+        public string Cusip { get; set; }
+        public string Isin { get; set; }
+
+    }
+}
